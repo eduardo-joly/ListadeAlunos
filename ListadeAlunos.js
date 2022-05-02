635ee252a2618bbc3c0683e040fe68f7856a9ae8
Eduardo Joly <eduardo.niceinfo@gmail.com>
2022-05-02 13:12:41 -0300

    Primeira mudança

--- ListadeAlunos.js
+++ ListadeAlunos.js
@@ -3,5 +3,10 @@ let numerodeAlunos = 10;
 for (let contador = 0; contador < numerodeAlunos; contador++) {
 
     console.log(contador)
+
+    if (contador == 0) {
+        console.log("o número atual é zero")
+    }
       
 }
+
