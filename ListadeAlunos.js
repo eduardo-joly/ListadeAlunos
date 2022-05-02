0ca6fa13850cab79cf1b2f2bfba5859b7a6c38d4
Eduardo Joly <eduardo.niceinfo@gmail.com>
2022-05-02 13:25:53 -0300

    terceira modificação

--- ListadeAlunos.js
+++ ListadeAlunos.js
@@ -2,16 +2,16 @@ let numerodeAlunos = 10;
 
 for (let contador = 0; contador < numerodeAlunos; contador++) {
 
-    console.log(contador)
+    // console.log(contador)
 
     if (contador == 0) {
         console.log("o número atual é zero")
     }
     else if (contador % 2 == 0) {
-        console.log("o número é par")
+        console.log("o número" + contador + "é par")
     }
     else{
-        console.log("o número é impar")
+        console.log(`o número ${contador} é impar`)
     }
       
 }
